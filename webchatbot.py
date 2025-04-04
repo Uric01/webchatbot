acd0244d7877b28b0ce0bfbcb185fef790f037c9
Zweli Mashego <uric01@live.com>
2025-04-04 18:44:35 +0200

    Update webchatbot.py

--- webchatbot.py
+++ webchatbot.py
@@ -1,5 +1,19 @@
-
+import sys
+import os
+import torch
+import textwrap
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQAWithSourcesChain
+import tenacity
+import nltk
+from langchain.vectorstores import FAISS
 import streamlit as st
+import getpass
+import os
+import httpx
 
 
 st.write("Hello, World")
